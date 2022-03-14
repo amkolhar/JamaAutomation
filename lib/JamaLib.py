ad31421b7b08dd9c2e42da51bb202b4e7c63a313
Atharv Kolhar <amkolhar@gmail.com>
2022-03-14 15:41:27 -0700

    Updated the Lib and created a logger for the tool

--- lib/JamaLib.py
+++ lib/JamaLib.py
@@ -4,30 +4,19 @@
 Jama Library
 """
 
-import logging
-import warnings
-
-from py_jama_rest_client.client import JamaClient
+import json
 
 import JamaAutomation.data.constants as const
 from JamaAutomation.data import config
+from JamaAutomation.lib import logger
+from JamaAutomation.py_jama_rest_client.client import JamaClient
+from JamaAutomation.py_jama_rest_client.core import Core
 
-warnings.filterwarnings("ignore")
 
-logger = logging.getLogger("JAMALIB")
-logger.setLevel(logging.INFO)
+def text_to_rich_text(text):
+    rich_text = "<p>%s</p>\n\n" % text
 
-handle = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                              "%Y-%m-%d %H:%M:%S")
-handle.setFormatter(formatter)
-handle.setLevel(logging.INFO)
-logger.addHandler(handle)
-
-log_handle = logging.FileHandler("JamaAutomation_log.txt")
-log_handle.setFormatter(formatter)
-log_handle.setLevel(logging.INFO)
-logger.addHandler(log_handle)
+    return rich_text
 
 
 class JamaLib:
@@ -36,10 +25,15 @@ class JamaLib:
                                             credentials=(config.CONFIG[const.USERNAME],
                                                          config.CONFIG[const.PASSWORD]),
                                             oauth=True)
+        self.core_auth_client = Core(config.CONFIG[const.COMPANY_JAMA_URL],
+                                     user_credentials=(config.CONFIG[const.USERNAME],
+                                                       config.CONFIG[const.PASSWORD]),
+                                     oauth=True)
         self.project = config.CONFIG[const.PROJECT]
         self.assigned_to = config.CONFIG[const.ASSIGNED_TO]
         self.testcase_ids = {}
         self.testsuite_location = config.TESTSUITE_LOCATION
+        self.logger = logger.jamalogger
 
     def create_folder(self, folder_details):
         """
@@ -54,7 +48,7 @@ class JamaLib:
                                                          child_item_type_id=folder_details["child_item_id"],
                                                          location={"item": folder_details[const.PARENT_LOCATION]},
                                                          fields=folder_details[const.FIELDS])
-        logger.info("Folder %s is created" % folder_details[const.FIELDS][const.NAME])
+        self.logger.info("Folder %s is created" % folder_details[const.FIELDS][const.NAME])
 
         return folder_api_id
 
@@ -72,19 +66,22 @@ class JamaLib:
                                                            location={
                                                                "item": testcase_details[const.PARENT_LOCATION]},
                                                            fields=testcase_details[const.FIELDS])
-            logger.info("Success : %s is created" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.info("Success : %s is created" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.info("JAMA RESPONSE : %s" % testcase_id)
+
+            return testcase_id
 
         except Exception as e:
-            logger.error(e)
-            logger.error(f"Error in creating the testcase : %s" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.error(e)
+            self.logger.error(f"Error in creating the testcase : %s" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.debug(e, exc_info=True)
             return None
-
-        return testcase_id
 
     def update_test_case(self, testcase_details):
         """
         testcase_details = {"parent_location": "int",
-                            "fields": dictionary of key:values for test_case}
+                            "fields": dictionary of key:value for test_case,
+                            "testcase_id": "int"}
         :param testcase_details:
         :return:
         """
@@ -96,11 +93,112 @@ class JamaLib:
                                                             location={
                                                                 "item": testcase_details[const.PARENT_LOCATION]},
                                                             fields=testcase_details[const.FIELDS])
-            logger.info("Success : %s is updated" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.info("Success : %s is updated" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.info("JAMA RESPONSE : %s" % jama_response)
 
         except Exception as e:
-            logger.error(e)
-            logger.error("Error in updating the testcase : %s" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.error(e)
+            self.logger.error("Error in updating the testcase : %s" % testcase_details[const.FIELDS][const.NAME])
+            self.logger.debug(e, exc_info=True)
+
+    def create_test_cycle(self, test_cycle_details, start_date, end_date):
+        """
+        test_cycle_details = {"testplan_id": "int",
+                              "name": "testcycle_name",
+                              "test_group": "int"
+                                }
+        :param end_date:
+        :param start_date:
+        :param test_cycle_details:
+        :return:
+        """
+
+        try:
+            response = self.basic_auth_client.post_testplans_testcycles(testplan_id=test_cycle_details["testplan_id"],
+                                                                        testcycle_name=test_cycle_details["name"],
+                                                                        start_date=start_date,
+                                                                        end_date=end_date,
+                                                                        testgroups_to_include=
+                                                                        test_cycle_details["test_group"])
+
+            self.logger.info(f"Testcycle id: {response}")
+
+        except Exception as e:
+            logger.jamalogger.warning("Error in creating Test Cycle")
+            logger.jamalogger.error(e)
+            raise Exception("There is error in creating Testcycle. Please check plan id")
+
+        return response
+
+    def update_test_run(self, testrun_details):
+        """
+
+        :param testrun_details: {"testrun_id": "int",
+                                  "fields": dictionary of key:value for test_run
+                                    }
+        :return:
+        """
+        test_run_id = testrun_details["testrun_id"]
+        data = {"fields": testrun_details["fields"]}
+        try:
+            self.basic_auth_client.put_test_run(test_run_id, json.dumps(data))
+
+        except Exception as e:
+            self.logger.warning("Error in updating the Test run result for %s" % test_run_id)
+            self.logger.error(e)
+            self.logger.debug(e, exc_info=True)
+
+    def get_test_group_id(self, test_plan_id):
+        response = self.basic_auth_client.get_test_groups(test_plan_id)
+
+        test_group_dict = {}
+
+        for i in response:
+            test_group_dict[i["name"]] = i["id"]
+
+        return test_group_dict
+
+    def create_test_group(self, test_plan_id, test_group_name):
+
+        testgroup_dict = self.get_test_group_id(test_plan_id)
+
+        if test_group_name not in testgroup_dict.keys():
+            self.basic_auth_client.post_test_group(test_plan_id, test_group_name)
+
+    def update_testgroup(self, testplan_id, testgroup_id, testcase_id_list):
+        res = f'testplans/{testplan_id}/testgroups/{testgroup_id}/testcases'
+
+        for i in testcase_id_list:
+            data = {"testCase": i}
+            response = self.core_auth_client.post(resource=res, json=data)
+            status = response.json()["meta"]["status"]
+
+            if status == "Created":
+                self.logger.info(f"Testcase {i} is added to testplan {testplan_id} "
+                                 f"under testgroup {testgroup_id}")
+
+            elif status == "Conflict":
+                self.logger.info(response.json()["meta"]["message"])
+
+    def get_testrun_id(self, test_cycle_id):
+
+        response = self.basic_auth_client.get_testruns(test_cycle_id=test_cycle_id)
+
+        test_run_testcase_dict = dict()
+        for testrun in response:
+            test_run_testcase_dict[testrun["fields"]["name"]] = testrun["id"]
+
+        return test_run_testcase_dict
+
+    def create_testcase_requirement_relationship(self, requirement_id, testcase_id):
+
+        try:
+            self.basic_auth_client.post_relationship(int(requirement_id), int(testcase_id), config.VERIFIED_BY)
+            logger.jamalogger.info("Mapped %s : %s" % (requirement_id, testcase_id))
+
+        except Exception as e:
+            self.logger.error(e)
+            self.logger.debug(str(e), exc_info=True)
 
 
 if __name__ == '__main__':
